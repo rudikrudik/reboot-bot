e9f870707f7ac93d76d2ba6cdbf5e9a7c61860e2
rudikrudik <rudik.login@gmail.com>
2025-08-03 15:04:42 +0300

    add deploy app

--- src/get_current_time.py
+++ src/get_current_time.py
@@ -3,6 +3,3 @@ from datetime import datetime
 
 def get_current_time() -> str:
     return datetime.now().strftime("%Y %m %d %H:%M")
-
-
-print(get_current_time())
--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import time
 from get_current_time import get_current_time
 from get_current_version import get_current_version
 
-
 while True:
     print(f"{get_current_version()} Current time is:", get_current_time(), flush=True)
     time.sleep(10)
-    
