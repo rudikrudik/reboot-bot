3e8b40dfef86725765a7ca301a198a4bc1aa9e90
rudikrudik <rudik.login@gmail.com>
2025-08-03 14:17:00 +0300

    add deploy app

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 
 while True:
-    print("v3 Current time is:", datetime.now(), flush=True)
+    print("v4 Current time is:", datetime.now(), flush=True)
     time.sleep(10)
     
