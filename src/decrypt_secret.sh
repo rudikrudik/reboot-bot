a964a162960843b87367f29c638745b7c14ffffc
rudikrudik <rudik.login@gmail.com>
2025-08-03 20:25:34 +0300

    add decrypt_secret.sh

--- src/decrypt_secret.sh
+++ src/decrypt_secret.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/ip_list.py ip_list.py.gpg
+--output ip_list.py ip_list.py.gpg
