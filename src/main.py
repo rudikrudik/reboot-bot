fd56023866a5d8df638bdfe67fb77b5220a8c4cf
rudikrudik <rudik.login@gmail.com>
2025-08-03 20:01:40 +0300

    add decrypt_secret.sh

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 import time
 from get_current_time import get_current_time
 from get_current_version import get_current_version
-from ip_list import ip
-
-for k, v in ip.items():
-    print(f"Pos: {k}, Ip: {v}")
-
 
 while True:
     print(f"{get_current_version()} Current time is:", get_current_time(), flush=True)
